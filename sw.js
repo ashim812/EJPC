c1bf2108e74a0b16177cccc61a79aeaa4d2c183a
Ashim Manna <ashimmanna182@gmail.com>
2025-12-18 15:52:20 +0530

    Update service worker cache version to v1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ejpc-cache-v2';
+const CACHE_NAME = 'ejpc-cache-v1';
 const ASSETS = [
     './',
     './index.html',
@@ -44,4 +44,5 @@ self.addEventListener('fetch', (e) => {
         })
     );
 });
+
 
