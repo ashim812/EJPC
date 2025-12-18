78bbb61869e5f78f382c5a19951abeaab9dae4b5
Ashim Manna <ashimmanna182@gmail.com>
2025-12-18 15:58:03 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,6 +13,7 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
+    self.skipWaiting();
     e.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
     );
@@ -44,5 +45,6 @@ self.addEventListener('fetch', (e) => {
         })
     );
 });
+
 
 
